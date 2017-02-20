0ffbfa3f10e5c7d824ec4a17129a990abd42989b
IvelinMariyanovIvanov <i.ivanov_1987@abv.bg>
2017-02-19 22:57:09 -0800

    a

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    public class Student
+    {
+        public string name;
+        public static int countStudents = 0;
+     
+
+        public Student(string name)
+        {
+            this.name = name;
+            countStudents++;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                Student currentStudent = new Student(command);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(Student.countStudents);
+        }
+    }
+}
